1060e7e9ab93b8d8e34d0e7e4df51eb4d1e883e5
Mike Echevarria <mike.echevarria@gmail.com>
2020-08-21 16:15:07 -0400

    configure vuex and router in object then use

--- src/main.js
+++ src/main.js
@@ -1,27 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
 import DropdownPlugin from 'bootstrap-vue'
 import ToastPlugin from 'bootstrap-vue'
 import VueMobileDetection from 'vue-mobile-detection'
 import 'perfect-scrollbar/dist/perfect-scrollbar'
 import '@popperjs/core/dist/esm/popper'
-import routes from './routes'
+import router from './app-router'
+import store from './app-store'
 
 Vue.config.productionTip = false
 
-Vue.use(VueRouter)
 Vue.use(DropdownPlugin)
 Vue.use(ToastPlugin)
 Vue.use(VueMobileDetection)
 
-const router = new VueRouter({
-  mode: 'history',
-  routes: routes,
-  linkActiveClass: 'c-sidebar-nav-link-primary'
-})
-
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app')
