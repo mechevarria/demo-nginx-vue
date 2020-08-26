5fdd7cb56a0fc1cbe7f74f98a8c5e11e8cde35ad
Mike Echevarria <mike.echevarria@gmail.com>
2020-08-26 16:01:56 -0400

    add proxy to jsonplaceholder

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
     devServer: {
-        port: 4200
+        port: 4200,
+        proxy: {
+            '/jsonplaceholder': {
+                target: 'http://jsonplaceholder.typicode.com',
+                secure: false,
+                pathRewrite: {
+                    '^/jsonplaceholder': ''
+                }
+            }
+        }
+    },
+    configureWebpack: {
+        devtool: 'source-map'
     }
 }
