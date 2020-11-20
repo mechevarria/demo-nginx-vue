c980b9bb6cd9d40760aec40aef7ba42894b19214
Mike Echevarria <mike.echevarria@gmail.com>
2020-11-20 11:15:21 -0500

    use 5000 as default port

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     devServer: {
-        port: 4200,
+        port: 5000,
         proxy: {
             '/jsonplaceholder': {
                 target: 'http://jsonplaceholder.typicode.com',
