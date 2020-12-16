1828fb2c4fae39b796837148ffc0c435126e0cd6
Mike Echevarria <mike.echevarria@gmail.com>
2020-12-15 22:10:08 -0500

    make dynamic route parsing

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="c-subheader justify-content-between px-3">
-    <ol class="breadcrumb border-0 m-0 px-0 px-md-3">
-      <li class="breadcrumb-item" v-if="nameNotHome">
-        <router-link to="/home">Home</router-link>
-      </li>
-      <li class="breadcrumb-item active">
-        <span>{{ $route.name }}</span>
+    <ol class="breadcrumb border-0 m-0">
+      <li
+        class="breadcrumb-item"
+        v-for="(route, index) in breadcrumbs"
+        :key="index"
+      >
+        <router-link v-if="index !== breadcrumbs.length -1" :to="{ name: route }">{{ upperFirst(route) }}</router-link>
+        <span v-else class="active">{{ upperFirst(route) }}</span>
       </li>
     </ol>
   </div>
@@ -13,11 +15,18 @@
 
 <script>
 export default {
-    name: 'AppBreadcrumbs',
-    computed : {
-      nameNotHome() {
-        return this.$route.name !== 'Home'
-      }
+  name: 'AppBreadcrumbs',
+  computed: {
+    breadcrumbs() {
+      const breadcrumbs = this.$route.fullPath.split('/')
+      breadcrumbs.shift()
+      return breadcrumbs
     }
+  },
+  methods: {
+    upperFirst(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+  }
 }
 </script>
